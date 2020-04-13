64015323ae0b833f88c130e992d16e65b207bfdc
jnchaitanya077 <chaitu.krish4@msitprogram.net>
2020-04-13 20:55:10 +0530

    assignment-done

--- lru/lru.py
+++ lru/lru.py
@@ -1,16 +1,35 @@
 class lru:
-    def __init__(self, size, data):
+
+    def __init__(self, size):
         self.size = size
-        self.data = data
+        self.lru = []
+        self.lruCache = {}
 
     def get(self, key, default=None):
-
-        pass
+        if key in self.lru:
+            return self.lruCache[key]
+        else:
+            return default
 
     def put(self, key):
 
-        pass
+        if(len(self.lru) < self.size):
+            if key in self.lru:
+                self.lru.remove(key)
+                self.lru.append(key)
+                self.lruCache[key] = "www."+str(key)+".com"
+            else:
+                self.lru.append(key)
+                self.lruCache[key] = "www."+str(key)+".com"
+        else:
+
+            var = self.lru.pop(0)
+            self.lru.append(key)
+            del self.lruCache[var]
+            self.lruCache[key] = "www."+str(key)+".com"
 
     def get_cache(self):
-
-        pass
+        ls = []
+        for key in reversed(self.lru):
+            ls.append(key+"-" + self.lruCache[key])
+        return ls
