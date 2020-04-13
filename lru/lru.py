26cbe268cf16dc1a14f27ca5cf35f9274783b03d
jnchaitanya077 <chaitu.krish4@msitprogram.net>
2020-04-13 16:54:43 +0530

    Written public methods

--- lru/lru.py
+++ lru/lru.py
@@ -2,3 +2,15 @@ class lru:
     def __init__(self, size, data):
         self.size = size
         self.data = data
+
+    def get(self, key, default=None):
+
+        pass
+
+    def put(self, key, data):
+
+        pass
+
+    def get_cache(self):
+
+        pass
