f3aa3835c8d5a202b26c18474fb08fcb22187503
jnchaitanya077 <55684213+jnchaitanya077@users.noreply.github.com>
2020-04-28 23:24:45 +0530

    Completed

--- Javascript/beancounting.js
+++ Javascript/beancounting.js
@@ -1,22 +1,23 @@
 function countBs(x) {
-    var c = 0
-    for (let i = 0; i < x.length; i++) {
-        if (x[i] === "B") {
-            console.log(x[i])
-            c++
-        }
+  var c = 0;
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] === "B") {
+      console.log(x[i]);
+      c++;
     }
-    return c
+  }
+  return c;
 }
 
 function countChar(newstring, character) {
-    var c = 0
-    for (let i = 0; i < newstring.length; i++) {
-        if (newstring[i] === character) {
-            c++
-        }
+  var c = 0;
+  for (let i = 0; i < newstring.length; i++) {
+    if (newstring[i] === character) {
+      c++;
     }
-    return c
-
+  }
+  return c;
 }
-console.log(countChar("erAAdgsAh", "A"))
+
+
+console.log(countChar("erAAdgsAh", "A"));
